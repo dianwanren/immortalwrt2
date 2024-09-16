6831b95aba8ae21058686e9937ff60ed5269ca2d
dianwanren <82783305+dianwanren@users.noreply.github.com>
2024-09-16 10:04:45 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -15,18 +15,11 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
- sed -i 's/192.168.1.1/192.168.88.33/g' package/base-files/files/bin/config_generate
-#
-# ------------------------------- Main source ends -------------------------------
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.88.33/g' package/base-files/files/bin/config_generate
 
-# ------------------------------- Other started -------------------------------
-#
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Apply patch
-# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
-#
-# ------------------------------- Other ends -------------------------------
-
+# Modify hostname
+sed -i 's/OpenWrt/Google/g' package/base-files/files/bin/config_generate
