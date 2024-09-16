66f193ad332bead0aac664141660b9eedb1d060e
dianwanren <82783305+dianwanren@users.noreply.github.com>
2024-09-16 17:20:47 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -12,6 +12,6 @@
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 # Add a feed source
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
